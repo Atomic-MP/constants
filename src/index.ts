dc139e0771f48ce0fa6e69c21f7f68065df82440
Miles <14067660+milesflo@users.noreply.github.com>
2019-06-25 10:41:16 -0700

    Adding max and min for hunger thirst and health

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,12 @@ interface IRole {
 const SALT_ROUNDS: number = 10;
 const HEADS: number       = 10;
 const HAIRS: number       = 24;
+const MAX_HEALTH: number  = 100;
+const MIN_HEALTH: number  = 0;
+const MAX_HUNGER: number  = 100;
+const MIN_HUNGER: number  = 0;
+const MAX_THIRST: number  = 100;
+const MIN_THIRST: number  = 0;
 const HAIR_COLORS: number = 7;
 const ROLES: IRole[] = [
   {
@@ -36,4 +42,10 @@ export default {
   HAIRS,
   HAIR_COLORS,
   ROLES,
+  MAX_HEALTH,
+  MIN_HEALTH,
+  MAX_HUNGER,
+  MIN_HUNGER,
+  MAX_THIRST,
+  MIN_THIRST,
 }
