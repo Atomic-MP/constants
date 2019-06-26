eb3ce0ddd44c7bf50125bb777d036897e9229953
Miles <14067660+milesflo@users.noreply.github.com>
2019-06-25 19:17:08 -0700

    Fixing types declaration

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,13 @@
-interface IRole {
-  role_id: number;
-  role_name: string;
+declare namespace Constants {
+  interface IRole {
+    role_id: number;
+    role_name: string;
+  }
+
+  interface IStatusRange {
+    MIN: number;
+    MAX: number;
+  }
 }
+
+export = Constants;
